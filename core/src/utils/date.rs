c5c944b50b5303b144fa04d5d90ef5f5c652f71d
Kinan Dak Albab <kinan.bab.14@gmail.com>
2024-04-18 14:05:24 -0400

    Remove date parsing PPR

--- core/src/utils/date.rs
+++ core/src/utils/date.rs
@@ -1,6 +1,5 @@
 use alohomora::bbox::BBox;
 use alohomora::policy::Policy;
-use alohomora::pure::PrivacyPureRegion;
 use chrono::NaiveDate;
 
 use crate::error::ServiceError;
@@ -11,14 +10,10 @@ pub fn parse_naive_date_from_opt_str<P: Policy>(date: Option<BBox<String, P>>, f
     match date {
         None => Err(ServiceError::InvalidDate),
         Some(date) => {
-            let date = date.into_ppr(PrivacyPureRegion::new(|date: String| {
-                let date = NaiveDate::parse_from_str(date.as_str(), fmt);
-                match date {
-                    Ok(date) => Ok(date),
-                    Err(_) => Err(ServiceError::InvalidDate),
-                }
-            }));
-            date.transpose()
+            match date.into_date(fmt) {
+                Ok(date) => Ok(date),
+                Err(_) =>  Err(ServiceError::InvalidDate),
+            }
         }
     }
 }
