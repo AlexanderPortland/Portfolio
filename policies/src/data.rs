da94cb7666c5d94ca906f5292ffdbded1c23da72
AlexanderPortland <alexanderportland1@gmail.com>
2024-09-14 16:53:14 -0400

    data policy update

--- policies/src/data.rs
+++ policies/src/data.rs
@@ -1,32 +1,41 @@
 use core::panic;
 
-use alohomora::{orm::ORMPolicy, policy::{AnyPolicy, FrontendPolicy, Policy, PolicyAnd}, AlohomoraType};
-use rocket::{data, figment::value::magic::Either};
+use alohomora::{orm::ORMPolicy, policy::{AnyPolicy, FrontendPolicy, Policy, PolicyAnd}, testing::TestContextData, AlohomoraType};
+use rocket::{data, figment::value::magic::Either, State};
+use sea_orm::{ConnectionTrait, Statement};
 use serde::Serialize;
 use mysql::prelude::Queryable;
 
 use crate::context::ContextDataTypeOut;
 
-
 #[derive(Clone, Serialize, Debug, PartialEq)]
 pub struct CandidateDataPolicy {
-    // you can only access sensitive candidate data (PLAIN or CIPHERTEXT) if: 
-    //      a) you are that candidate
-    //      b) you are an admin
-
-    candidate_id: Option<i32>,
+    session_id: Option<String>, // only set for data coming from client POST
+    candidate_id: Option<i32>,  // only set for data coming from DB
 }
+
+// (->) candidate data can enter the system
+//     a. as post data (we have session_id cookie) <- FrontendPolicy
+//     b. as query from DB (we have candidate_id)  <- ORMPolicy
+
+// (<-) candidate data can be leaked
+//     a. as rendering (we have session_id, pk?)
+//     b. into pcr (we don't care to validate)
+//     c. into db  (^^)
 
 impl CandidateDataPolicy {
     pub fn new(candidate_id: Option<i32>) -> Self {
-        CandidateDataPolicy{ candidate_id }
+        CandidateDataPolicy{ 
+            session_id: None,
+            candidate_id 
+        }
     }
 }
 
 impl Default for CandidateDataPolicy {
     fn default() -> Self {
         println!("defaulting!!");
-        CandidateDataPolicy { candidate_id: None }
+        CandidateDataPolicy { session_id: None, candidate_id: None }
     }
 }
 
@@ -38,26 +47,50 @@ impl Policy for CandidateDataPolicy {
         }
     }
 
+    // right client (cand_id) render -> ok
+    // any admin render -> ok
+    // right client (in session) db -> ok
+    // custom region -> okay
+    // EVERYTHING ELSE -> nuh uh
+
     fn check(&self, context: &alohomora::context::UnprotectedContext, reason: alohomora::policy::Reason<'_>) -> bool {
+        println!("thank you sir! you've given me {:?}", context);
+        let context: &ContextDataTypeOut = if let Some(test) = context.downcast_ref::<TestContextData<ContextDataTypeOut>>() {
+            // test.0
+            // FIXME: how to downcast to testcontext data here
+            println!("test context data");
+            todo!()
+        } else {
+            println!("real context data");
+            context.downcast_ref().unwrap()
+        };
+
         match reason {
             // 0. we trust the custom reviewers
             alohomora::policy::Reason::Custom(_) => return true,
-            // 1. all DB writes are okay
-            alohomora::policy::Reason::DB(_, _) => return true,
-            // 2. if they're trying to render it
-            alohomora::policy::Reason::TemplateRender(s) => {
-                if let Some(owner_candidate_id) = self.candidate_id {
-                    // 2a. check if they're owner
+            // 1. if writing to DB, make sure it's from the same session as data
+            alohomora::policy::Reason::DB(_, _) => {
+                // return true;
+                if let Some(session_id) = self.session_id.clone(){
+                    return session_id == context.session_id.clone().unwrap();
                 }
-
-                // 2b. check if they're admin
-
-                return false;
             }
-            a => {
-                return false;
+            // 2. if rendering, we must either be a) an admin, or b) the right candidate
+            alohomora::policy::Reason::TemplateRender(_) => {
+                if let Some(candidate_id) = self.candidate_id {
+                    // candidate check
+                }
+                // admin check
+                todo!()
+                // let res = context.conn.execute(Statement::from_string(
+                //     context.conn.get_database_backend(),
+                //     String::from(""),
+                // )).await?;
             }
+            _ => return false,
         }
+
+        return false;
     }
 
     fn join(&self, other: alohomora::policy::AnyPolicy) -> Result<alohomora::policy::AnyPolicy, ()> {
@@ -73,15 +106,18 @@ impl Policy for CandidateDataPolicy {
     }
 
     fn join_logic(&self, other: Self) -> Result<Self, ()> where Self: Sized {
-        let p = if self.candidate_id == other.candidate_id {
+        let (mut candidate_id, mut session_id) = (None, None);
+        if self.candidate_id == other.candidate_id {
             // if they have the same id, keep it
-            CandidateDataPolicy{ candidate_id: self.candidate_id }
-        } else {
-            // if not, no users should be allowed to access the data
-            CandidateDataPolicy{ candidate_id: None }
-        };
-
-        Ok(p)
+            candidate_id = self.candidate_id;
+        }
+        if self.session_id == other.session_id {
+            session_id = self.session_id.clone();
+        }
+        Ok(CandidateDataPolicy{
+            candidate_id,
+            session_id,
+        })
     }
 }
 
@@ -99,14 +135,18 @@ impl ORMPolicy for CandidateDataPolicy {
             }
         };
 
+        println!("found candidate id {candidate_id}");
+
         CandidateDataPolicy { 
             candidate_id: Some(candidate_id),
+            session_id: None,
         }
     }
 
     fn empty() -> Self where Self: Sized {
         CandidateDataPolicy{
             candidate_id: None,
+            session_id: None,
         }
     }
 }
@@ -126,9 +166,12 @@ impl FrontendPolicy for CandidateDataPolicy {
             // cookie id is a session id which maps in the sessions db table to candidate_id which is what we want
             Some(session_id) => {
                 println!("yahoo i got id {session_id}");
-                // let s = id.to_string().parse().unwrap();
-                todo!()
-                // CandidateDataPolicy::new(Some(s))
+                let session_id = Some(session_id.to_string());
+                println!("(or as a string) {:?}", session_id);
+                CandidateDataPolicy {
+                    session_id,
+                    candidate_id: None,
+                }
             },
             None => {
                 println!("no such luck with the id cookie strategy");
