2bc0e2fa2edc7ee778558788208e301172244817
AlexanderPortland <alexanderportland1@gmail.com>
2024-09-15 17:46:37 -0400

    candidate data id conversion partially working

--- policies/src/data.rs
+++ policies/src/data.rs
@@ -10,9 +10,12 @@ use crate::context::ContextDataTypeOut;
 
 #[derive(Clone, Serialize, Debug, PartialEq)]
 pub struct CandidateDataPolicy {
-    session_id: Option<String>, // only set for data coming from client POST
-    // get
-    candidate_id: Option<i32>,  // only set for data coming from DB
+    // only set for data coming from client POST
+    session_id: Option<String>, 
+    
+    // only set for data coming from DB
+    candidate_id: Option<i32>,    // (candidate table)
+    application_id: Option<i32>,  // (application table)
 }
 
 // (->) candidate data can enter the system
@@ -28,7 +31,8 @@ impl CandidateDataPolicy {
     pub fn new(candidate_id: Option<i32>) -> Self {
         CandidateDataPolicy{ 
             session_id: None,
-            candidate_id 
+            candidate_id,
+            application_id: None,
         }
     }
 }
@@ -36,35 +40,44 @@ impl CandidateDataPolicy {
 impl Default for CandidateDataPolicy {
     fn default() -> Self {
         println!("defaulting!!");
-        CandidateDataPolicy { session_id: None, candidate_id: None }
+        CandidateDataPolicy { session_id: None, candidate_id: None, application_id: None }
     }
 }
 
-fn does_session_exist(is_admin: bool, db: &DatabaseConnection, session_id: String, candidate_id: Option<i32>) -> bool {
-    println!("seeing if session exists w/ as admin {is_admin}, session_id {session_id}, candidate_id: {:?}", candidate_id);
+fn does_session_exist(is_admin: bool, db: &DatabaseConnection, session_id: String, candidate_id: Option<i32>, application_id: Option<i32>) -> bool {
+    let application_id = if let Some(id) = application_id { 
+        println!("already have app id {id}");
+        Some(id) 
+    } else if let Some(candidate_id) = candidate_id {
+        println!("trying to get app id for cand {candidate_id}");
+        let result = rocket::tokio::task::block_in_place(||{
+            let res = db.query_all(Statement::from_string(
+                    db.get_database_backend(),
+                    // format!("select * from admin_session where id = {};", session_id),
+                    format!("select * from application where candidate_id={candidate_id};"),
+                ));
+            rocket::tokio::runtime::Handle::current().block_on(res).unwrap()
+        }).first().unwrap().try_get::<i32>("", "id");
+        println!("result {:?}", result);
+        match result {
+            Ok(ok) => Some(ok),
+            Err(_) => None
+        }
+    } else {
+        None
+    };
+
+    println!("seeing if session exists w/ as admin {is_admin}, session_id {session_id}, candidate_id: {:?}, application_id: {:?}", candidate_id, application_id);
     let session_id = sea_orm::prelude::Uuid::parse_str(session_id.as_str()).unwrap();
     let table_name = if is_admin { String::from("admin_session") } else { String::from("session") };
-    let candidate_id_phrase = if let Some(candidate_id) = candidate_id {
-        // TODO: validate session based on candidate_id
-        // format!(" and candidate_id = {}", candidate_id)
-        // have to do another db query here to get gov id from candidate_id
-        // let candidate_gov_id = rocket::tokio::task::block_in_place(||{
-        //     let res = db.query_all(Statement::from_string(
-        //             db.get_database_backend(),
-        //             // format!("select * from admin_session where id = {};", session_id),
-        //             // format!("select * from application where id={candidate_id} ;"),
-        //             format!("select * from application;"),
-        //         ));
-        //     rocket::tokio::runtime::Handle::current().block_on(res).unwrap().first().unwrap().try_get::<i32>("", "candidate_id")
-        // }).unwrap();
-        String::from("")
-        // format!(" and candidate_id = {}", candidate_gov_id)
+    let id_phrase = if let Some(application_id) = application_id {
+        format!(" and candidate_id = {}", application_id)
     } else { String::from("") };
     let result = rocket::tokio::task::block_in_place(||{
         let res = db.query_all(Statement::from_string(
                 db.get_database_backend(),
                 // format!("select * from admin_session where id = {};", session_id),
-                format!("select * from {table_name} where id=0x{:x} {candidate_id_phrase};", session_id.as_u128()),
+                format!("select * from {table_name} where id=0x{:x} {id_phrase};", session_id.as_u128()),
             ));
         rocket::tokio::runtime::Handle::current().block_on(res).unwrap()
     });
@@ -142,7 +155,7 @@ impl Policy for CandidateDataPolicy {
                 // }
 
                 // admin check
-                if does_session_exist(true, &context.conn, context.session_id.clone().unwrap(), None) {
+                if does_session_exist(true, &context.conn, context.session_id.clone().unwrap(), None, None) {
                     return true;
                 }
 
@@ -154,7 +167,7 @@ impl Policy for CandidateDataPolicy {
                 // candidate check
                 if let Some(_) = self.candidate_id {
                     println!("from cand check");
-                    return does_session_exist(false, &context.conn, context.session_id.clone().unwrap(), self.candidate_id);
+                    return does_session_exist(false, &context.conn, context.session_id.clone().unwrap(), self.candidate_id, None);
                 }
                 return false
             },
@@ -180,7 +193,7 @@ impl Policy for CandidateDataPolicy {
     }
 
     fn join_logic(&self, other: Self) -> Result<Self, ()> where Self: Sized {
-        let (mut candidate_id, mut session_id) = (None, None);
+        let (mut candidate_id, mut session_id, mut application_id) = (None, None, None);
         if self.candidate_id == other.candidate_id {
             // if they have the same id, keep it
             candidate_id = self.candidate_id;
@@ -188,43 +201,64 @@ impl Policy for CandidateDataPolicy {
         if self.session_id == other.session_id {
             session_id = self.session_id.clone();
         }
+        if self.application_id == other.application_id {
+            application_id = self.application_id.clone();
+        }
         Ok(CandidateDataPolicy{
             candidate_id,
             session_id,
+            application_id
         })
     }
 }
 
 impl ORMPolicy for CandidateDataPolicy {
     fn from_result(result: &sea_orm::prelude::QueryResult) -> Self {
-        let candidate_id = match result.try_get::<i32>("", "candidate_id") {
-            Ok(r) => {
-                println!("from 1st");
-                r
-            },
-            Err(_) => {
-                // so either we are in the candidate table where it's just called `id`
-                // todo!();
-                println!("from 2nd");
-                match result.try_get("", "id") {
-                    Ok(r) => r,
-                    // or something went wrong
-                    Err(e) => panic!("issue making candidate data policy from db {e}"),
-                }
+        let policy = if let Ok(r) = result.try_get::<i32>("", "ip_address") {
+            // in session table
+            println!("in session table w/ result {:?}", r);
+            todo!()
+        } else if let Ok(r) = result.try_get::<i32>("", "password") {
+            // in application table
+            println!("in application table w/ result {:?}", r);
+            match result.try_get::<i32>("", "id") {
+                Ok(application_id) => CandidateDataPolicy { 
+                    application_id: Some(application_id),
+                    session_id: None,
+                    candidate_id: None,
+                },
+                Err(e) => panic!("{:?}", e),
+            }
+        } else if let Ok(r) = result.try_get::<i32>("", "candidate_id") {
+            // in parent table
+            println!("in parent table w/ result {:?}", r);
+            CandidateDataPolicy { 
+                application_id: Some(r),
+                session_id: None,
+                candidate_id: None,
+            }
+        } else {
+            // in the candidate table table
+            println!("in candidate table");
+            match result.try_get("", "id") {
+                Ok(candidate_id) => CandidateDataPolicy { 
+                    candidate_id: Some(candidate_id),
+                    session_id: None,
+                    application_id: None,
+                },
+                // or something went wrong
+                Err(e) => panic!("issue making candidate data policy from db {e}"),
             }
         };
 
-        println!("found candidate id {candidate_id}");
-
-        CandidateDataPolicy { 
-            candidate_id: Some(candidate_id),
-            session_id: None,
-        }
+        println!("found policy {:?}", policy);
+        policy
     }
 
     fn empty() -> Self where Self: Sized {
         CandidateDataPolicy{
             candidate_id: None,
+            application_id: None,
             session_id: None,
         }
     }
@@ -250,6 +284,7 @@ impl FrontendPolicy for CandidateDataPolicy {
                 CandidateDataPolicy {
                     session_id,
                     candidate_id: None,
+                    application_id: None,
                 }
             },
             None => {
